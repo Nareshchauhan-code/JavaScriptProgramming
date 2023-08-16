1daae138f203835253689c05d185993ed5be4ebd
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-16 18:36:13 +0530

    Loops in JavaScript

---
+++ Conditions.js
@@ -0,0 +1,8 @@
+let required = false;
+if(required==true){
+console.log("Condition is True");
+}
+else if(required!=true){
+
+    console.log('Condition is False');
+}
---
+++ WhileLoop.js
@@ -0,0 +1,11 @@
+let num =0;
+let count =0;
+while(num<10){
+
+    num++;
+
+    console.log('Welcome to JavaScript');
+
+    count++;
+}
+console.log(count);
