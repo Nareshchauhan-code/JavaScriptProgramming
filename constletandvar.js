44554447c2688337cad77cea941905c33f64d4d4
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-21 13:29:16 +0530

    Arrow function

--- constletandvar.js
+++ constletandvar.js
@@ -6,3 +6,14 @@ var data = 678900;
 
 console.log(name);
 console.log(data);
+
+async function showServiceCost() {
+    try {
+       let user = await getUser(100);
+       let services = await getServices(user);
+       let cost = await getServiceCost(services);
+       console.log(`The service cost is ${cost}`);
+    } catch(error) {
+       console.log(error);
+    }
+}
